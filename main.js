99f9ca721da5eae5f8507ec15a68717bec7abe1c
Pranav Pise <itspranavpise@gmail.com>
2025-04-10 12:11:03 +0530

    creating a scene

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import * as THREE from "three";
+
+const seene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGL3DRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
